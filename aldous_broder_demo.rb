d0484b453fc9bb00a72c6b89e6e6ec06ebb864b4
Mark Mansour <mark@stateofflux.com>
2016-10-08 14:42:23 -0700

    added Aldous Broder

--- aldous_broder_demo.rb
+++ aldous_broder_demo.rb
@@ -5,6 +5,10 @@ require_relative 'aldous_broder'
 grid = ColoredGrid.new(40, 40)
 AldousBroder.on(grid)
 
+start = grid[grid.rows / 2, grid.columns / 2]
+
+grid.distances = start.distances
+
 filename = "aldous_broder.png"
 grid.to_png.save(filename)
 puts "saved to #{filename}"
